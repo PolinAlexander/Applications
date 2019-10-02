dd7fc0a558a9f59802afe5a4344fa741e869133f
PolinAlexander <msk.slim.msk@gmail.com>
2019-10-02 19:25:15 +0300

    add ver control Web and WinForm app

---
+++ AirportsRelationsApp.Win/WinApplication.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace AirportsRelationsApp.Win {
+    partial class AirportsRelationsAppWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new AirportsRelationsApp.Module.AirportsRelationsAppModule();
+            this.module4 = new AirportsRelationsApp.Module.Win.AirportsRelationsAppWindowsFormsModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // AirportsRelationsAppWindowsFormsApplication
+            // 
+            this.ApplicationName = "AirportsRelationsApp";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.objectsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.AirportsRelationsAppWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.AirportsRelationsAppWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private AirportsRelationsApp.Module.AirportsRelationsAppModule module3;
+        private AirportsRelationsApp.Module.Win.AirportsRelationsAppWindowsFormsModule module4;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+    }
+}
